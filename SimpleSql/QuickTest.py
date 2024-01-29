5ac6b735ce6d1806fc49c2a2fe37872a2997527d
Kalash004 <kalashnikov004@gmail.com>
2024-01-29 21:59:44 +0100

    testing

--- SimpleSql/QuickTest.py
+++ SimpleSql/QuickTest.py
@@ -7,7 +7,15 @@ class Test(SimpleSql.Base):
     stuff = SimpleSql.Param(SimpleSql.Types.STRING, SimpleSql.Constraints.UNIQUE, SimpleSql.Constraints.NOT_NULL)
 
 
+class Test2(SimpleSql.Base):
+    table_name = "Lol"
+    test2_Id = SimpleSql.Param(SimpleSql.Types.INT, SimpleSql.Constraints.PK)
+    stuff = SimpleSql.Param(SimpleSql.Types.STRING, SimpleSql.Constraints.UNIQUE, SimpleSql.Constraints.NOT_NULL)
+    stuff2 = SimpleSql.Param(SimpleSql.Types.STRING, SimpleSql.Constraints.UNIQUE, SimpleSql.Constraints.NOT_NULL)
+
+
 if __name__ == "__main__":
     test = Test(test_Id=1, stuff="Hello")
+    test2 = Test2(test2_Id=1, stuff="Hello", stuff2="Watsup ?")
     app = SimpleSql.App()
     app.start()
