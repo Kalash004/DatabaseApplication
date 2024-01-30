929244e5704a253fedb30c9db0aa68a4666d9078
Kalash004 <kalashnikov004@gmail.com>
2024-01-30 20:04:05 +0100

    Add insert, select, update to builder Added SQL commands build for insert, select, update

--- SimpleSql/Core/QueryBuilder/QueryBuilder.py
+++ SimpleSql/Core/QueryBuilder/QueryBuilder.py
@@ -2,44 +2,52 @@ from SimpleSql.Models.Models.SQLHolder import SimpleSQLHolder as Holder
 
 
 class SimpleQueryBuilder:
+    def __init__(self):
+        self.changeable_sign = "?"
 
     def build_sql(self, tables):
         built = dict()
-        for table_name, table in tables.items():
-            ddl = self.__build_creation(table_name, table)
-            insert = self.__build_insert(table_name, table)
-            select = self.__build_select(table_name, table)
-            update = self.__build_update(table_name, table)
-            delete = self.__build_delete(table_name, table)
+        for table_name, _table in tables.items():
+            table_copy = _table.struct.copy()
+            self.__remove_tablename(table_copy)
+            ddl = self.__build_creation(table_name, table_copy)
+            insert = self.__build_insert(table_name)
+            select = self.__build_select(table_name)
+            update = self.__build_update(table_name, table_copy)
+            delete = self.__build_delete(table_name, table_copy)
             built[table_name] = Holder(table_name, table_builder_DDL=ddl, insert=insert, select=select, update=update,
                                        delete=delete)
 
-    def __build_creation(self, table_name, _table):
-        # remove table name from structure
-        table_copy = _table.struct.copy()
-        self.__remove_tablename(table_copy)
-        query = ""
+    def __build_creation(self, table_name, table_copy):
+        middle = ""
         for attribute in table_copy:
             name = attribute[0]
             data_type = attribute[1].datatype
             constraints = attribute[1].constraints
-            query += f"{name} "
-            query = self.__set_contraints(query, data_type, constraints)
+            middle += f"{name} "
+            middle = self.__set_contraints(middle, data_type, constraints)
 
-        query = query.rstrip(' ,')
-        mysql_creation_q = (f"CREATE TABLE {table_name}("
-                            f"{query}"
-                            f")")
-        return mysql_creation_q
+        middle = middle.rstrip(' ,')
+        query = (f"CREATE TABLE {table_name}("
+                 f"{middle}"
+                 f")")
+        return query
 
-    def __build_insert(self, table_name, table):
-        pass
+    def __build_insert(self, table_name):
+        return f"INSERT INTO {table_name} VALUES ({self.changeable_sign})"
 
-    def __build_select(self, table_name, table):
-        pass
+    def __build_select(self, table_name):
+        return f"SELECT {self.changeable_sign} FROM {table_name}"
 
-    def __build_update(self, table_name, table):
-        pass
+    def __build_update(self, table_name, table_copy):
+        middle = ""
+        for attribute in table_copy:
+            name = attribute[0]
+            middle += f" {name}={self.changeable_sign} "
+        query = (f"UPDATE {table_name}"
+                 f"SET{middle}"
+                 f"WHERE {self.changeable_sign}")
+        return query
 
     def __build_delete(self, table_name, table):
         pass
